5912f4311810c8b09156404874a163a743291976
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2022-12-20 11:58:44 +0500

    code refactor

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const formData = [
     statement: 'You name',
     type: 'text',
     name: 'fname',
+    id: null,
     placeholder: 'first name',
     value: ''
   },
@@ -39,41 +40,53 @@ const showForm = () => {
   });
 }
 
+const getQuestionWrapper = () => document.createElement('div');
+const getLabel = (text, id = '') => {
+  const ele = document.createElement('label');
+  ele.innerText = text;
+  ele.setAttribute('for', id)
+  return ele;
+};
+const getQuestionStatement = (text) => {
+  const ele = document.createElement('h5');
+  ele.setAttribute('class', 'form-')
+  ele.innerText = text;
+  return ele;
+};
+
 const createTextInput = (data) => {
-  let questionContaioner = document.createElement('div');
-  questionContaioner.setAttribute('class', 'form-group');
-  let questionStatement = document.createElement('h5');
-  questionStatement.innerText = data.statement;
-  questionContaioner.appendChild(questionStatement);
+  let questionContaioner = getQuestionWrapper();
+  let questionStatement = getLabel(data.statement, data.id || data.name);
   let field = document.createElement('input');
+  questionContaioner.appendChild(questionStatement);
+  questionContaioner.appendChild(field);
+  questionContaioner.setAttribute('class', 'form-group');
   field.setAttribute('class', 'form-control');
   field.setAttribute('type', 'text')
   field.setAttribute('placeholder', data.placeholder);
   field.setAttribute('name', data.name);
   field.setAttribute('value', data.value);
-  questionContaioner.appendChild(field);
+  field.setAttribute('id', data.id || data.name);
   return questionContaioner;
 }
 const createRadioInput = (data) => {
-  let questionContaioner = document.createElement('div');
-  questionContaioner.setAttribute('class', 'form-group');
-  let questionStatement = document.createElement('h5');
-  questionStatement.innerText = data.statement;
-  questionContaioner.appendChild(questionStatement);
+  let questionContaioner = getQuestionWrapper();
+  let questionStatement = getQuestionStatement(data.statement);
   let field = document.createElement('input');
+  questionContaioner.appendChild(questionStatement);
+  questionContaioner.appendChild(field);
+  questionContaioner.setAttribute('class', 'form-group');
   field.setAttribute('type', 'radio')
   field.setAttribute('name', data.name);
   field.setAttribute('value', data.value);
-  questionContaioner.appendChild(field);
   return questionContaioner;
 }
 const createCheckInput = (data) => {
-  let questionContaioner = document.createElement('div');
-  questionContaioner.setAttribute('class', 'form-group');
-  let questionStatement = document.createElement('h5');
-  questionStatement.innerText = data.statement;
-  questionContaioner.appendChild(questionStatement);
+  let questionContaioner = getQuestionWrapper();
+  let questionStatement = getQuestionStatement(data.statement);
   let field = document.createElement('input');
+  questionContaioner.setAttribute('class', 'form-group');
+  questionContaioner.appendChild(questionStatement);
   field.setAttribute('type', 'checkbox')
   field.setAttribute('name', data.name);
   field.setAttribute('value', data.value);
@@ -81,22 +94,22 @@ const createCheckInput = (data) => {
   return questionContaioner;
 }
 const createRangeInput = (data) => {
-  let questionContaioner = document.createElement('div');
-  questionContaioner.setAttribute('class', 'form-group');
-  let questionStatement = document.createElement('h5');
-  questionStatement.innerText = data.statement;
-  questionContaioner.appendChild(questionStatement);
+  let questionContaioner = getQuestionWrapper();
+  let questionStatement = getLabel(data.statement, data.id || data.name);
   let field = document.createElement('input');
+  questionContaioner.setAttribute('class', 'form-group');
+  questionContaioner.appendChild(questionStatement);
   field.setAttribute('type', 'range')
   field.setAttribute('name', data.name);
   field.setAttribute('value', data.value);
+  field.setAttribute('id', data.id || data.name)
   questionContaioner.appendChild(field);
   return questionContaioner;
 }
 const createHeading = (data) => {
-  let questionContaioner = document.createElement('div');
-  questionContaioner.setAttribute('class', 'form-group');
+  let questionContaioner = getQuestionWrapper();
   let field = document.createElement(`h${data.size}`);
+  questionContaioner.setAttribute('class', 'form-group');
   field.innerText = data.value;
   questionContaioner.appendChild(field);
   return questionContaioner;
