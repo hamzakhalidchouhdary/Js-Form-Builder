8befa5c51ea097584ebc5f6a7e9f8c7b4bd335f9
Hamza Khalid Chouhdary <hamzah.khalid@goicon.com>
2023-01-13 11:44:20 +0500

    created add options method

--- assets/js/addNewQuestion.js
+++ assets/js/addNewQuestion.js
@@ -55,4 +55,32 @@ const closeQuestionPopUp = () => {
   resetQuestionForm();
 }
 
+const createOptionsElement = (data) => {
+  let questionContaioner = getQuestionWrapper();
+  questionContaioner.setAttribute('class', 'form-group');
+  let field = document.createElement('input');
+  let label = getLabel(data.label, data.id || data.name)
+  questionContaioner.appendChild(field);
+  questionContaioner.appendChild(label);
+  field.setAttribute('type', 'checkbox');
+  field.setAttribute('checked', true);
+  field.setAttribute('name', data.name);
+  field.setAttribute('value', data.value);
+  field.setAttribute('id', data.id || data.name)
+  return questionContaioner;
+}
+
+const addOptions = () => {
+  const optLabel = document.getElementsByName('label')[0].value;
+  const optValue = document.getElementsByName('value')[0].value;
+  const selectedType = document.getElementsByName('type')[0].value;
+  const optElement = createOptionsElement({
+    name: 'options', 
+    statement: '',
+    value: `${optLabel}:${optValue}`,
+    label: optLabel
+  });
+  document.getElementById('options-field').appendChild(optElement);
+  }
+
 const resetQuestionForm = () => document.getElementById('newQuestionForm').reset();
